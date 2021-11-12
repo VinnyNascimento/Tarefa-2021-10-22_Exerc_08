b3807fd96d82c6f3cf03ab8f582c101317d50adf
VinnyNascimento <91098218+VinnyNascimento@users.noreply.github.com>
2021-11-12 19:11:46 -0300

    Add files via upload

---
+++ Exerc_08/Exerc_08/Exerc_08/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exerc_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region declarações & instâncias
+            Calculo s;
+            s = new Calculo();
+            #endregion
+
+            #region Introducao
+            Console.Write("Descubra qual o Maior Numero");
+            Console.ReadLine();
+            #endregion
+
+            #region entrada
+            Console.Write("Informe o 1° numero: ");
+            s.setNum1(int.Parse(Console.ReadLine()));
+
+            Console.Write("Informe o 2° numero: ");
+            s.setNum2(int.Parse(Console.ReadLine()));
+            #endregion
+
+            #region processo
+            s.calcular();
+            #endregion
+
+        }
+    }
+}
