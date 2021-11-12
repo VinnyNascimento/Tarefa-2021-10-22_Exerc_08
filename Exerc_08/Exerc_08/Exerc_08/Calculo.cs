c62bea9ae9001501316e503847d2690e83907ca6
VinnyNascimento <91098218+VinnyNascimento@users.noreply.github.com>
2021-11-12 03:02:26 -0300

    Add files via upload

---
+++ Exerc_08/Exerc_08/Exerc_08/Calculo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerc_08
+{
+    class Calculo
+    {
+        #region atributos
+        private double num1;
+        private double num2;
+        #endregion
+
+        #region construtores
+        public Calculo()
+        {
+            this.num1 = 0;
+            this.num2 = 0;
+        }
+
+        public Calculo(double num1, double num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+        #endregion
+
+        #region métodos de interface (Setter's & Getter's)
+        public void setNum1(double num1)
+        {
+            this.num1 = num1;
+        }
+
+        public void setNum2(double num2)
+        {
+            this.num2 = num2;
+        }
+
+        public double getNum1()
+        {
+            return this.num1();
+        }
+
+        public double getNum2()
+        {
+            return this.num2;
+        }
+        #endregion
+
+        #region métodos funcionais
+        public void calcular()
+        {
+            if (this.num1 > this.num2)
+            {
+                getNum1(); = this.num1;
+                getNum2(); = this.num2;
+            }
+            else
+            {
+                getNum2(); = this.num1;
+                getNum1(); = this.num2;
+            }
+            
+        }
+        #endregion
+    }
+}
---
+++ Exerc_08/Exerc_08/Exerc_08/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exerc_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region declarações & instâncias
+            Calculo s;
+            s = new Calculo();
+            #endregion
+
+            #region Introducao
+            Console.Write("Descubra qual número é maior, ou se saõ idênticos");
+            Console.ReadLine();
+            #endregion
+
+            #region entrada
+            Console.Write("Informe o 1° número ");
+            s.setNum1(double.Parse(Console.ReadLine()));
+
+            Console.Write("Informe o 2° número ");
+            s.setNum2(double.Parse(Console.ReadLine()));
+            #endregion
+
+            #region processo
+            s.calcular();
+            #endregion
+
+            #region saída
+            Console.WriteLine("O número {0} é maior que o número {1}", s.getNum1(), s.getNum2());
+            #endregion
+        }
+    }
+}
